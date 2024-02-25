eae73a902ba8680f7a21bacb57466bd312f263fc
kowoa <koadahoakodah@gmail.com>
2024-02-25 02:19:51 -0500

    Fix glsl filetypedetect autocmd overwriting other filetypedetect autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,9 +2,14 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.cmd([[
-  augroup filetypedetect
-    autocmd!
-    autocmd BufRead,BufNewFile *.comp setfiletype glsl
-  augroup END
-]])
+local function augroup(name)
+  return vim.api.nvim_create_augroup("custom_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  group = augroup("glsl_filetypedetect"),
+  pattern = { "comp" },
+  callback = function()
+    vim.bo.filetype = "glsl"
+  end,
+})
