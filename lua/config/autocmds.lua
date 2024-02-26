701f3cde1a4e79fa160c86927761b586c3ecbcc6
kowoa <koadahoakodah@gmail.com>
2024-02-26 02:09:52 -0500

    Use autocmd directly instead of augroup

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,14 +2,4 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-local function augroup(name)
-  return vim.api.nvim_create_augroup("custom_" .. name, { clear = true })
-end
-
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  group = augroup("glsl_filetypedetect"),
-  pattern = { "comp" },
-  callback = function()
-    vim.bo.filetype = "glsl"
-  end,
-})
+vim.cmd([[autocmd BufNewFile,BufRead *.comp set filetype=glsl]])
