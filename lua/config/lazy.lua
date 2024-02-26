d887cfd6c055c70d3074211e8e4ec757dcab2522
kowoa <koadahoakodah@gmail.com>
2024-02-26 14:49:04 -0500

    Add extras for rust and dotfiles

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,9 +12,8 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.vscode" },
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.util.dot" },
     -- import/override with your plugins
     { import = "plugins" },
   },
